df0a573f0fb88e703ff95d5304c8c5b446983afb
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-05-14 12:38:19 +0100

    New Changes

--- server/app.js
+++ server/app.js
@@ -3,18 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//var url = require('url')
-
-//const { fileURLToPath } = require('url');
-//const { importMeta } = require('node:import-meta');
-
-//const { url } = importMeta;
-//const filePath = fileURLToPath(url);
-//console.log(filePath);
+var fs = require('fs')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var clientsRouter = require('./routes/client')
+var clientRouter = require('./routes/client');
 
 var app = express();
 
@@ -25,20 +18,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(__dirname)
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handle all other routes by serving the index.html file
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
+// app.post('/', (req, res) => {
+//   fs.readFile(path.join(__dirname, 'client/build/clients.json'), (err, data) => {
+//       if (data && !err) {
+//           var parsedData = JSON.parse(data)
+//           parsedData.push(req.body)
+//           console.log(parsedData)
+//           fs.writeFile(path.join(__dirname, 'client/build/clients.json'), JSON.stringify(parsedData, null, 2), (err) => {
+//               if (err) {
+//                   return res.status(500).json({msg: "Unable to save"})
+//               }
+//                   return res.status(200).json({msg:"Saved successfully"})
+              
+//           })
+//       }else{
+//           var parsedData = []
+//           parsedData.push(req.body)
+//           console.log(parsedData)
+//           fs.writeFile(path.join(__dirname, 'client', 'build', 'clients.json'), JSON.stringify(parsedData, null, 2), (err) => {
+//               if (err) {
+//                   return res.status(500).json({msg: "Unable to save"})
+//               }
+//                   return res.status(200).json({msg:"Saved successfully"})
+              
+          
+//       })
+//   }
+//   })
+// })
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/clients', clientsRouter);
+app.use('/client', clientRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
